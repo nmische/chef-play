0abef79e58178dfc611880f31e173fe61caa2af3
Jake Davis <jake.davis5989@gmail.com>
2012-10-12 23:26:07 -0700

    Initial commit!

---
+++ README.md
@@ -0,0 +1,7 @@
+Play Cookbook
+=============
+
+This is a Chef cookbook to install and configure the Play framework
+(www.playframework.org).
+
+
---
+++ metadata.rb
@@ -0,0 +1,14 @@
+# metadata.rb
+# Author: Jake Davis (jake.davis5989@gmail.com)
+#
+
+maintainer       "Jake Davis"
+maintainer_email "jake.davis5989@gmail.com"
+license          "MIT"
+description      "Installs Play framework"
+version          "0.1.0"
+
+# I'll add more!
+%w{ ubuntu }.each do |os|
+    supports os
+end
