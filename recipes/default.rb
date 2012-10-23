03a010973cf43f3597bc0837a028f11b44699bb3
Jake Davis <jake.davis5989@gmail.com>
2012-10-22 18:43:48 -0700

    basic install stuffs

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,21 +3,22 @@
 
 package "zip"
 
+execute "install-play" do
+    user "root"
+    cwd node['play']['install_dir']
+    command <<-EOH
+    unzip play-#{node[:play][:version]}.zip
+    export PATH=$PATH:#{node['play']['install_dir']}/play-#{node['play']['version']}
+    rm play-#{node['play']['version']}.zip
+    EOH
+    action :nothing
+end
+
 remote_file "#{node[:play][:install_dir]}/play-#{node[:play][:version]}.zip" do
     source "http://download.playframework.org/releases/play-2.0.4.zip"
     #source node[:play][:source_uri]
     mode "0644"
     notifies :run, "execute[install-play]", :immediately
     action :create_if_missing
-end
-
-execute "install-play" do
-    user "root"
-    cwd node[:play][:install_dir]
-    command <<-EOH
-    unzip play-#{node[:play][:version]}.zip
-    export PATH=$PATH:#{node[:play][:install_dir]}/play-#{node[:play][:version]}
-    EOH
-    action :nothing
 end
 
